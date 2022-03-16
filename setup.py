4ac48e91b8525c8c22628cb13ccf06d5e6419bfc
dadafari <34658977+dadafari@users.noreply.github.com>
2022-03-16 04:00:06 +0100

    change install_require to django 3 versions

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'Django>=1.11,<2.0'
+        'Django>=1.11,<4.0'
     ],
     classifiers=[
         'Environment :: Web Environment',
